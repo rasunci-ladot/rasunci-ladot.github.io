f4a506bf359064aff05e5f53a382a31ae84c07dc
rasunci-ladot <romeo.asuncion@lacity.org>
2020-02-25 15:58:45 -0800

    Refactor. Added calendar

--- js/teems.common.js
+++ js/teems.common.js
@@ -8,56 +8,169 @@ class ToolbarButton extends React.Component {
     }
 
     render() {
-        return e('div', {
-            className: this.props.className,
+        return e('button', {
+            type: 'button',
             id: this.props.id,
-            onClick: this.props.handler
+            className: this.props.className,
+            onClick: this.props.handleOnClick,
             },
-            this.props.face
+            e('img', {
+                className: this.props.imgClassName,
+                src: this.props.img,
+            }),
         );
     }
 }
 
-function makeToolbar() {
-    function handleOpenPanel() {
-        document.getElementById('div-validate').style.visibility = 'visible';
-        //document.getElementById('input-validate').value = '';
-        //document.getElementById('input-validate').focus();
+class ToolbarFragment extends React.Component {
+    constructor(props) {
+        super(props);
     }
 
-    function handleGoTo() {
-        window.location.href = '/home';
+    render() {
+        let buttons = this.props.buttons;
+        let toolbar = [];
+        for (let i=0; i<buttons.length; i++) {
+            toolbar.push(e(
+                ToolbarButton, {
+                    key: i, // Warning suppression
+                    id: buttons[i].id,
+                    className: buttons[i].className,
+                    handleOnClick: buttons[i].handleOnClick,
+                    img: buttons[i].img,
+                    imgClassName: buttons[i].imgClassName,
+                }),
+            );
+        }
+
+        return e(React.Fragment, null, toolbar);
+    }
+}
+
+class CloseButton extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleOnClick = this.handleOnClick.bind(this);
     }
 
-    return e(
-        React.Fragment,
-        null,
-        e(ToolbarButton, {
-            className: 'toolbar-button-large toolbar-size-large',
-            id: 'div-toolbar-00',
-            face: e('img', {
-                className: 'toolbar-size-large',
-                src: '/img/la-logo.svg'
+    handleOnClick() {
+        document.getElementById(this.props.panelId).style.visibility = 'hidden';
+    }
+
+    render() {
+        return e('button', {
+            type: 'button',
+            className: 'close-button close-button-size',
+            onClick: this.handleOnClick,
+            },
+            e('img', {
+                className: 'close-button-size',
+                src: '/img/window-close.png',
             })
-        }),
-        e(ToolbarButton, {
-            className: 'toolbar-button toolbar-size',
-            id: 'div-toolbar-01',
-            handler: handleOpenPanel,
-            face: e('img', {
-                className: 'toolbar-size',
-                src: '/img/new-work-order.svg'
-            })
-        }),
-        e(ToolbarButton, {
-            className: 'toolbar-button toolbar-size',
-            id: 'div-toolbar-02',
-            handler: handleGoTo,
-            face: e('img', {
-                className: 'toolbar-size',
-                src: '/img/search-work-orders.svg'
-            })
-        }),
-    );
+        );
+    }
+}
+
+class PanelFragment extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return e(
+            React.Fragment,
+            null,
+            e(CloseButton, {
+                panelId: this.props.panelId,
+            }),
+            this.props.elements,
+        );
+    }
+}
+
+class Calendar extends React.Component {
+    constructor(props) {
+        super(props);
+        let sysDate = new Date();
+        this.state = {
+            month: sysDate.getMonth(),
+            year: sysDate.getFullYear(),
+        };
+    }
+
+    render() {
+        let months = [
+            {name: 'January', days: 31},
+            {name: 'February', days: 28},
+            {name: 'March', days: 31},
+            {name: 'April', days: 30},
+            {name: 'May', days: 31},
+            {name: 'June', days: 30},
+            {name: 'July', days: 31},
+            {name: 'August', days: 31},
+            {name: 'September',days: 30},
+            {name: 'Octoboer', days: 31},
+            {name: 'November', days: 30},
+            {name: 'December', days: 31},
+        ];
+        let calDate = new Date(months[this.state.month].name+'/1/'+this.state.year);
+        let calDay = calDate.getDay(); // Day of week
+        console.log('month='+months[this.state.month].name);
+        console.log('calDay='+calDay);
+
+        // Leap year check
+        let year = this.state.year;
+        let leapYear = false;
+        if (year % 400 == 0) {leapYear = true;} // console.log('400: Leap year');}
+        else {
+            if (year % 100 == 0) {console.log('100: Not a leap year');}
+            else {
+                if (year % 4 == 0) {leapYear = true;} // console.log('4: Leap year');}
+                else {console.log('Default: Not a leap year');}
+            }
+        }
+        if (leapYear) {
+            console.log('days='+months[1].days);
+            months[1].days = 29;
+            console.log('days='+months[1].days);
+        }
+
+        return e('div', {});
+    }
+}
+
+class DatePickerFragment extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+
+    render() {
+        let date = this.props.date;
+
+        let datePicker = [
+            e('input', {
+                type: 'text',
+                id: date.inputId,
+                className: date.inputClassName,
+            }),
+            e('button', {
+                type: 'button',
+                id: date.buttonId,
+                className: date.buttonClassName,
+                },
+                e('img', {
+                    className: date.imgClassName,
+                    img: date.imgSrc,
+                }),
+            ),
+            e('div', {
+                id: date.calId,
+                className: date.calClassName,
+            }),
+        ];
+
+        return e(React.Fragment, null, datePicker);
+    }
 }
 
--- js/teems.requester.js
+++ js/teems.requester.js
@@ -3,39 +3,40 @@
 class CandidateItem extends React.Component {
     constructor(props) {
         super(props);
-        this.handleClick = this.handleClick.bind(this);
-        this.handleMouseOver = this.handleMouseOver.bind(this);
-        this.handleMouseOut = this.handleMouseOut.bind(this);
+        this.handleOnClick = this.handleOnClick.bind(this);
+        this.handleOnMouseOver = this.handleOnMouseOver.bind(this);
+        this.handleOnMouseOut = this.handleOnMouseOut.bind(this);
         this.state = {
-            fontSetting: this.props.className,
-            x: null,
-            y: null,
+            isMouseOver: false,
         }
     }
 
-    handleMouseOver() {
-        this.setState({fontSetting: this.props.hilites});
-        this.setState({x: this.props.x});
-        this.setState({y: this.props.y});
+    handleOnMouseOver() {
+        this.setState({isMouseOver: true});
     }
 
-    handleMouseOut() {
-        this.setState({fontSetting: this.props.className});
+    handleOnMouseOut() {
+        this.setState({isMouseOver: false});
     }
 
-    handleClick() {
-        document.getElementById('div-validate').style.visibility = 'hidden';
-        window.view.zoom = 18;
-        window.view.center = [this.state.x, this.state.y];
+    handleOnClick() {
+        //document.getElementById('div-location').style.visibility = 'hidden';
+        //window.view.zoom = 18;
+        window.view.center = [this.props.x, this.props.y];
     }
 
     render() {
+        let fontSetting = this.props.className;
+        if (this.state.onMouseOver) {
+            fontSetting += ' .hilites';
+        }
+
         return e('li', {
-            className: this.state.fontSetting,
             id: this.props.id,
-            onClick: this.handleClick,
-            onMouseOver: this.handleMouseOver,
-            onMouseOut: this.handleMouseOut,
+            className: fontSetting,
+            onClick: this.handleOnClick,
+            onMouseOver: this.handleOnMouseOver,
+            onMouseOut: this.handleOnMouseOut,
             },
             this.props.candidate
         );
@@ -47,7 +48,7 @@ class CandidateList extends React.Component {
         super(props);
         this.updateCandidates = this.updateCandidates.bind(this);
         this.state = {
-            candidates: [],
+            candidates: [], // Use state to store candidates and render list (since parent is stateless)
         };
     }
 
@@ -59,8 +60,7 @@ class CandidateList extends React.Component {
         }
         let addresses = {};
         let ordered = [];
-        let i;
-        for (i=0; i<len; i++) {
+        for (let i=0; i<len; i++) {
             let address = json.candidates[i].address;
             // Create a list of unique addresses
             if (typeof(addresses[address]) === 'undefined') {
@@ -76,12 +76,10 @@ class CandidateList extends React.Component {
     render() {
         let ordered = this.state.candidates;
         let candidateList = [];
-        let i;
-        for (i=0; i<ordered.length; i++) {
+        for (let i=0; i<ordered.length; i++) {
             candidateList.push(e(CandidateItem, {
                     key: i, // Suppress warnings
                     className: this.props.itemClassName,
-                    hilites: this.props.itemHilites,
                     candidate: ordered[i].address,
                     x: ordered[i].x,
                     y: ordered[i].y,
@@ -91,8 +89,8 @@ class CandidateList extends React.Component {
         }
 
         return e('ul', {
-            className: this.state.className,
             id: this.props.id,
+            className: this.state.className,
             },
             candidateList
         );
@@ -102,15 +100,15 @@ class CandidateList extends React.Component {
 class CandidateForm extends React.Component {
     constructor(props) {
         super(props);
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleOnChange = this.handleOnChange.bind(this);
+        this.handleOnSubmit = this.handleOnSubmit.bind(this);
         this.state = {
             buttonIsDisabled: true,
             location: '',
         };
     }
 
-    handleChange(event) {
+    handleOnChange(event) {
         let locLength = event.target.value.length;
         if (locLength > 0) {
             this.setState({buttonIsDisabled: false});
@@ -121,20 +119,20 @@ class CandidateForm extends React.Component {
         this.setState({location: event.target.value});
     }
 
-    handleError(statusText) {
+    handleOnError(statusText) {
         console.log(statusText);
     }
 
-    handleSubmit(event) {
+    handleOnSubmit(event) {
         event.preventDefault();
 
-        let url = this.props.arcgisURL;
+        let url = this.props.arcgis.url;
         let query = this.state.location;
         if (query.match(/los angeles/i)) {
-            url += query + this.props.arcgisFormat;
+            url += query + this.props.arcgis.format;
         }
         else {
-            url += query + this.props.arcgisCity + this.props.arcgisFormat;
+            url += query + this.props.arcgis.city + this.props.arcgis.format;
         }
     
         let promise = new Promise(function(resolve, reject) {
@@ -157,111 +155,184 @@ class CandidateForm extends React.Component {
     }
 
     render() {
-        return e('form', {onSubmit: this.handleSubmit},
+        let dateRange = this.props.dateRange;
+        //console.log('CandidateForm.render: dateRange='+dateRange);
+        let startDate = this.props.startDate;
+        let endDate = this.props.endDate;
+        let location = this.props.location;
+        let validate = this.props.validate;
+
+        return e('form', {onSubmit: this.handleOnSubmit},
+            e('label', {id: dateRange.labelId}, dateRange.labelText),
+            e('label', {id: startDate.labelId}, startDate.labelText),
             e('input', {
-                className: this.props.inputClassName,
-                id: this.props.inputId,
-                onChange: this.handleChange,
+                type: 'text',
+                id: startDate.inputId,
+            }),
+            e('button', {
+                type: 'button',
+                id: startDate.buttonId,
+                },
+                e('img', {
+                    className: startDate.imgClassName,
+                    src: startDate.imgSrc,
+                }),
+            ),
+            e('label', {id: endDate.labelId}, endDate.labelText),
+            e('input', {
+                type: 'text',
+                id: endDate.inputId,
+            }),
+            e('button', {
+                type: 'button',
+                id: endDate.buttonId,
+                },
+                e('img', {
+                    className: endDate.imgClassName,
+                    src: endDate.imgSrc,
+                }),
+            ),
+            e('label', {id: location.labelId}, location.labelText),
+            e('input', {
+                type: 'text',
+                id: location.inputId,
             }),
             e('button', {
                 type: 'submit',
-                className: this.props.buttonClassName,
-                id: this.props.buttonId,
-                disabled: this.state.buttonIsDisabled,
+                id: validate.buttonId,
                 },
-                this.props.buttonFace
+                validate.buttonText,
             ),
         );
     }
 }
 
-class CloseButton extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleClick = this.handleClick.bind(this);
-    }
-
-    handleClick() {
-        document.getElementById(this.props.panel).style.visibility = 'hidden';
-    }
-
-    render() {
-        return e('button', {
-            type: 'button',
-            id: this.props.id,
-            className: 'close-button close-button-size',
-            onClick: this.handleClick,
-            },
-            e('img', {
-                className: 'close-button-size',
-                src: '/img/window-close.png',
-            })
-        );
-    }
-}
-
-class SimplePanel extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return e(
-            React.Fragment,
-            null,
-            e(CloseButton, {
-                id: this.props.closeButtonId,
-                panel: this.props.id,
-            }),
-            this.props.elements,
-        );
-    }
-}
 
 function makeCandidatePanel() {
     // Create a pointer to CandidateList
     let listRef = React.createRef();
 
+    let dateRange = {
+        labelId: 'label-date-range',
+        labelText: 'Enter Date Range',
+    };
+    //console.log('makeCandidatePanel: dateRange.labelId='+dateRange.labelId);
+    //console.log('makeCandidatePanel: dateRange.labelText='+dateRange.labelText);
+
+    let startDate = {
+        labelId: 'label-start-date',
+        labelText: 'Start Date',
+        inputId: 'input-start-date',
+        buttonId: 'button-start-date',
+        imgClassName: 'cal-button',
+        imgSrc: '/img/foo.png',
+    };
+
+    let endDate = {
+        labelId: 'label-end-date',
+        labelText: 'End Date',
+        inputId: 'input-end-date',
+        buttonId: 'button-end-date',
+        imgClassName: 'cal-button',
+        imgSrc: '/img/foo.png',
+    };
+
+    let location = {
+        labelId: 'label-location',
+        labelText: 'Enter Location',
+        inputId: 'input-location',
+    };
+
+    let validate = {
+        buttonId: 'button-validate',
+        buttonDisabled: true,
+        buttonText: 'VALIDATE',
+    };
+
+    let arcgis = {
+        url: 'https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/findAddressCandidates?SingleLine=',
+        city: ',los angeles,ca',
+        format: '&f=pjson',
+    };
+
     let elements = [
-        e('div', {
-            key: 'labelAddress', // Warning suppression
-            id: 'div-validate-text',
-            className: 'fonts-std-c toolbar-font',
-            },
-            'Enter Location'
-        ),
         e(CandidateForm, {
-            key: 'candidateForm', // Warning suppression
-            inputId: 'input-validate',
-            inputClassName: 'fonts-std-c',
-            buttonId: 'button-validate',
-            buttonClassName: 'fonts-std-c toolbar-font',
-            buttonFace: 'VALIDATE',
-            listRef: listRef, // Pass listRef pointer to CandidateForm so it can update CandidateList
-            arcgisURL: 'https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/findAddressCandidates?SingleLine=',
-            arcgisCity: ',los angeles,ca',
-            arcgisFormat: '&f=pjson',
+            key: 1, // Warning suppression
+            dateRange: dateRange,
+            startDate: startDate,
+            endDate: endDate,
+            location: location,
+            validate: validate,
+            arcgis: arcgis,
+            listRef: listRef, // Allow CandidateForm to update CandidateList
         }),
         e(CandidateList, {
-            key: 'candidateList', // Warning suppression
+            key: 2, // Warning suppression
             ref: listRef,
-            id: 'list-validate',
+            id: 'list-location',
             className: 'fonts-std',
             itemClassName: 'fonts-std',
-            itemHilites: 'fonts-std hilite',
-        })
+        }),
     ];
 
-    ReactDOM.render(e(SimplePanel, {
-            id: 'div-validate',
-            elements: elements
-        }),
-        document.getElementById('div-validate')
-    );
+    ReactDOM.render(e(PanelFragment, {panelId: 'div-location', elements: elements}), document.getElementById('div-location'));
+}
+
+function makeToolbar() {
+    let buttons = [{
+            id: 'button-toolbar-00',
+            className: 'toolbar-button-large toolbar-button-size-large',
+            img: '/img/la-logo.svg',
+            imgClassName: 'toolbar-button-size-large',
+        }, {
+            id: 'button-toolbar-01',
+            className: 'toolbar-button toolbar-button-size',
+            img: '/img/new-work-order.svg',
+            imgClassName: 'toolbar-button-size',
+        }, {
+            id: 'button-toolbar-02',
+            className: 'toolbar-button toolbar-button-size',
+            img: '/img/search-work-orders.svg',
+            imgClassName: 'toolbar-button-size',
+        },
+    ];
+
+    ReactDOM.render(e(ToolbarFragment, {buttons: buttons}), document.getElementById('div-toolbar'));
 }
 
 function pageInit() {
-    ReactDOM.render(e(makeToolbar, {}), document.getElementById('div-toolbar'));
+    let date = new Date();
+    console.log('date='+date);
+    //let newDate = new Date('February 1, 2020');
+    let newDate = new Date('2/1/2020');
+    console.log('newDate='+newDate);
+    console.log('newDate: day='+newDate.getDay());
+    let checkDate = new Date();
+    console.log('checkDate='+checkDate);
+
+    let sysDate = new Date();
+    let year = sysDate.getFullYear();
+    console.log('year='+year);
+    let month = sysDate.getMonth();
+    month++;
+    console.log('month='+month);
+    let helperDate = new Date(month+'/1/'+year);
+    let day = helperDate.getDay();
+    console.log('day='+day);
+    console.log('month/day/year='+month+'/1/'+year+', day='+day);
+
+    if (year % 400 == 0) {console.log('400: leap year');}
+    else {
+        if (year % 100 == 0) {console.log('100: not leap year');}
+        else {
+            if (year % 4 == 0) {console.log('4: leap year');}
+            else {console.log('last: not leap year');}
+        }
+    }
+
+    makeToolbar();
     makeCandidatePanel();
+
+    ReactDOM.render(e(Calendar, {}), document.getElementById('div-wo'));
 }
 
